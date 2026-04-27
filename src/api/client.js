e5ea51dbc5cde6afda0e149436fa131e4494d970
Lazytodie11 <yipeng.li666@gmail.com>
2026-04-26 20:00:06 -0500

    Use VITE_API_URL env variable for API base URL

--- src/api/client.js
+++ src/api/client.js
@@ -1,11 +1,10 @@
 import axios from 'axios'
 
 const client = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080',
   headers: { 'Content-Type': 'application/json' },
 })
 
-// 自动在每个请求里加 Authorization header
 client.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
   if (token) {
@@ -14,7 +13,6 @@ client.interceptors.request.use((config) => {
   return config
 })
 
-// 401 自动跳转到登录页
 client.interceptors.response.use(
   (res) => res,
   (err) => {
